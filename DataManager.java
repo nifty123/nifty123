bfd265ed04049f4ab8c72f00c931f0c144720288
nifty123 <99277785+nifty123@users.noreply.github.com>
2022-03-03 23:12:27 -0500

    Add files via upload

---
+++ DataManager.java
@@ -0,0 +1,23 @@
+package application;
+//declare class
+public class DataManager {
+String Dtext = "";
+//Empty constructor
+DataManager() {}
+//implement the method getHello()
+public String getHello() {
+return "Hello";
+}
+//implement the method getHello()
+public String getHowdy() {
+return "Howdy";
+}
+//implement the method getChineese()
+public String getChineese() {
+return "Chineese";
+}
+//implement the method setText(String)
+public void setText(String text) {
+this.Dtext = text;
+}
+}
